d68cc42c738a8da35ee54cc95682fdcb2d33e256
Prakash Mondal <prakashmondal8967@gmail.com>
2021-07-11 04:14:10 +0530

    Added rank of matrix C program

--- Q13.Rank-of-matrix-Dynamic.c
+++ Q13.Rank-of-matrix-Dynamic.c
@@ -1,76 +1,143 @@
+/* Find Rank of a Matrix */
+/* RANK.C */
+
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
-int main()
+#include <math.h>
+
+int R, C;
+int i, j;
+
+void display(int **, int, int);
+void input(int **, int, int);
+int Rank_Mat(int **, int, int);
+void swap(int **, int, int, int);
+
+/* This function exchange two rows of a matrix */
+
+void swap(int **mat, int row1, int row2, int col)
 {
-    int i, j;
-    int **A;
-    A = (int **)malloc(3 * sizeof(int *));
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < col; i++)
     {
-        A[i] = malloc(3 * sizeof(int));
+        int temp = mat[row1][i];
+        mat[row1][i] = mat[row2][i];
+        mat[row2][i] = temp;
     }
+}
 
-    printf("Enter the values of a 3*3 Matrix :\n");
+/* This function find rank of matrix */
 
-    int flag = 0;
-    for (i = 0; i < 3; i++)
+int Rank_Mat(int **mat, int row1, int col1)
+{
+    int r, c;
+    for (r = 0; r < col1; r++)
     {
-        for (j = 0; j < 3; j++)
+        display(mat, R, C);
+        printf("\n");
+        if (mat[r][r]) // Diagonal element is not zero
         {
-            scanf("%d", &A[i][j]);
-            if (A[i][j] > flag)
-                flag = 1;
+            for (c = 0; c < row1; c++)
+                if (c != r)
+                {
+                    /* Make all the elements above and below the current principal
+                 diagonal element zero */
+
+                    double ratio = mat[c][r] / mat[r][r];
+                    for (i = 0; i < col1; i++)
+                        mat[c][i] -= ratio * mat[r][i];
+                }
         }
-    }
-    printf("\nRank of the matrix is : ");
-    if (flag == 0)
-    {
-        printf("0");
-    }
-    else
-    {
-        int d = A[0][0] * (A[1][1] * A[2][2] - A[2][1] * A[1][2]) - A[0][1] * (A[1][0] * A[2][2] - A[2][0] * A[1][2]) + A[0][2] * (A[1][0] * A[2][1] - A[2][0] * A[1][1]);
-        if (d != 0)
-        {
-            printf("3");
-        }
+
+        /* Principal Diagonal elment is zero */
+
         else
         {
-            int deter = A[0][0] * A[1][1] - A[1][0] * A[0][1];
-            if (deter != 0)
+            for (c = r + 1; c < row1; c++)
+                if (mat[c][r])
+                {
+                    /*  Find non zero elements in the same column */
+                    swap(mat, r, c, col1);
+                    break;
+                }
+
+            if (c == row1)
             {
-                printf("2");
+                --col1;
+
+                for (c = 0; c < row1; c++)
+                    mat[c][r] = mat[c][col1];
             }
-            else
-            {
-                int deter1 = A[1][0] * A[2][1] - A[2][0] * A[1][1];
-                if (deter1 != 0)
-                {
-                    printf("2");
-                }
-                int deter2 = A[0][1] * A[1][2] - A[1][1] * A[0][2];
-                if (deter2 != 0)
-                {
-                    printf("2");
-                }
-                int deter3 = A[2][2] * A[1][1] - A[2][1] * A[1][2];
-                if (deter3 != 0)
-                {
-                    printf("2");
-                }
-                else
-                    printf("1");
-            }
+            --r;
         }
     }
+    return col1;
+}
+
+/* Output function */
+
+void display(int **mat, int row, int col)
+{
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf("%4d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/* Input function */
+
+void input(int **mat, int row, int col)
+{
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf(" Enter Value for position of mat[%d][%d] : ", i + 1, j + 1);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+/* main function */
+
+int main()
+{
+
+    int rank;
+    printf("\n Enter number of rows : ");
+    scanf("%d", &R);
+    printf(" Enter number of columns : ");
+    scanf("%d", &C);
+    printf("\n");
+
+    int **mat;
+    mat = (int **)malloc(sizeof(int *) * R);
+
+    for (i = 0; i < R; i++)
+        mat[i] = (int *)malloc(sizeof(int) * C);
+
+    input(mat, R, C);
+    printf("\n Row of the matrix is : %d", R);
+    printf("\n Column of the matrix is : %d \n", C);
+
+    printf("\n Your matrix is as follows:\n\n");
+    display(mat, R, C);
+
+    //Calculating the rank of the matrix ....
+
+    printf("\n Row reduced operations are as follows :\n\n");
+    rank = Rank_Mat(mat, R, C);
+    printf(" Rank of the above matrix is : %d\n\n", rank);
 
     // Free the memory...
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < C; i++)
     {
-        free(A[i]);
+        free(mat[i]);
     }
-    free(A);
+    free(mat);
 
     return 0;
 }
