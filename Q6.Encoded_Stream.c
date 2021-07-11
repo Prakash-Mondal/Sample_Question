feeeb029ce7351eb4d90c4604160aecf8ef151cc
Prakash Mondal <prakashmondal8967@gmail.com>
2021-07-11 14:51:42 +0530

    Modified Q6.Encoded_stream.c

--- Q6.Encoded_Stream.c
+++ Q6.Encoded_Stream.c
@@ -1,4 +1,58 @@
+/* General Method*/
+
 #include <stdio.h>
+#include <string.h>
+void bStream(int c)
+{
+    int binary = 0, rem = 0, place = 1;
+    while (c != 0)
+    {
+        rem = c % 2;
+        c = c / 2;
+        binary = binary + (rem * place);
+        place = place * 10;
+    }
+    printf("%d", binary);
+}
+int main()
+{
+    char a[100], b[100];
+    int i, c;
+    printf("Enter a string containing alphabets only :\n");
+    gets(a);
+    strcpy(b, a);
+    strrev(b);
+    if (strcmp(a, b) == 0)
+    {
+        printf("\nString is palindrome\n\n");
+        printf("encodedStream = ");
+        for (i = 0; a[i] != '\0'; i++)
+        {
+            c = a[i];
+            bStream(c);
+            if (a[i + 1] != '\0')
+                printf("[1]");
+        }
+    }
+    else
+    {
+        printf("\nString is not palindrome\n\n");
+        printf("encodedStream = ");
+        for (i = 0; a[i] != '\0'; i++)
+        {
+            c = a[i];
+            bStream(c);
+            if (a[i + 1] != '\0')
+                printf("[0]");
+        }
+    }
+
+    return 0;
+}
+
+/* Another method by using Shift operator*/
+
+/*#include <stdio.h>
 #include <string.h>
 void bStream(int c)
 {
@@ -48,4 +102,4 @@ int main()
     }
 
     return 0;
-}
+}*/
